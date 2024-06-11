5393b0367158465f2cc3e2358ab9bbf177ae8f4f
Mananmishra2004 <137518804+Mananmishra2004@users.noreply.github.com>
2024-06-11 14:20:34 +0530

    Internship application website

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+            // Add event listeners to the CTA buttons
+document.querySelectorAll('.cta button').forEach(button => {
+    button.addEventListener('click', () => {
+        // Add your logic here for what happens when the button is clicked
+       alert("open a link in new tab");
+
+       
+    });
+});
